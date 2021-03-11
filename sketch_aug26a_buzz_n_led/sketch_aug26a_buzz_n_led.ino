8d0ba228c3fc581e9819c42b04dcfc536c6b3343
dheeraj <66857987+telladheerajnaidu@users.noreply.github.com>
2021-03-11 16:31:51 +0530

    Add files via upload

---
+++ sketch_aug26a_buzz_n_led/sketch_aug26a_buzz_n_led.ino
@@ -0,0 +1,25 @@
+const int buzzer1 =  4;
+const int buzzer2 =  0;
+
+void setup() {
+  pinMode(buzzer1, OUTPUT);
+  pinMode(buzzer2, OUTPUT);
+  pinMode(13, OUTPUT);
+       
+}
+
+void loop(){
+  
+  digitalWrite(buzzer1, HIGH);
+  delay(200);
+  digitalWrite(buzzer2, LOW);
+  delay(200);
+  digitalWrite(buzzer1, LOW);
+  delay(200);
+  digitalWrite(buzzer2, HIGH);
+  delay(200);  
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
